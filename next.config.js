97629c06cdeac1105a02b5bc0da4c6f463575034
kalloc <kalloc@users.noreply.github.com>
2023-02-03 01:32:59 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*', // automatically handles all locales
+        destination: 'https://httpbin.org/:path*', // automatically passes the locale on
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
